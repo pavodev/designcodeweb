d82b08e1b6c4e803500962cb083e5db113bea3d2
Ivan Pavic <ivan.pavic@usi.ch>
2021-10-27 17:01:47 +0200

    Implemented global styles

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -1,6 +1,7 @@
 import React from "react"
-import { Link } from "gatsby"
 import styled from "styled-components"
+import { themes } from "../styles/ColorStyles"
+import { H1, MediumText } from "../styles/TextStyles"
 
 function HeroSection() {
   return (
@@ -38,16 +39,10 @@ const TextWrapper = styled.div`
   gap: 30px;
 `
 
-const Title = styled.h1`
-  font-weight: bold;
-  font-size: 60px;
-  color: white;
+const Title = styled(H1)`
+  color: ${themes.dark.text1};
 `
 
-const Description = styled.p`
-  font-weight: normal;
-  font-size: 17px;
-  line-height: 130%;
-`
+const Description = styled(MediumText)``
 
 export default HeroSection
