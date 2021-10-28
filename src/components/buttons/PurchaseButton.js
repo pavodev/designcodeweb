02c37cfee06cf9fcf4a84215bdbf9adfc1714276
Ivan Pavic <ivan.pavic@usi.ch>
2021-10-28 15:43:19 +0200

    implemented mockup animations

--- src/components/buttons/PurchaseButton.js
+++ src/components/buttons/PurchaseButton.js
@@ -10,7 +10,7 @@ export default function PurchaseButton(props) {
     <Link to="/page-2">
       <Wrapper>
         <IconWrapper>
-          <Icon src="/images/icons/credit.svg" />
+          <Icon src="/images/icons/credit.svg" className="icon" />
           <Ring src="/images/icons/icon-ring.svg" />
         </IconWrapper>
         <TextWrapper>
@@ -37,6 +37,22 @@ const Wrapper = styled.div`
   //align-content: center; // Only works if the child elements together are < than the container
   align-items: center; // That's why we use align-items
   gap: 20px;
+
+  *,
+  & {
+    transition: 0.8s cubic-bezier(0.075, 0.82, 0.165, 1);
+  }
+
+  :hover {
+    box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.1),
+      0px 30px 60px rgba(23, 0, 102, 0.5),
+      inset 0px 0px 0px 0.5px rgba(255, 255, 255, 0.5); // Inner shadow doesn't show on Chrome, that's why we use the inset
+    transform: translateY(-3px);
+
+    .icon {
+      transform: scale(1.2);
+    }
+  }
 `
 
 const TextWrapper = styled.div`
@@ -64,15 +80,24 @@ const IconWrapper = styled.div`
   justify-content: center;
   align-content: center;
   justify-self: center; // Applies to the first column
-`
 
-const Icon = styled.img`
-  width: 29px;
-  height: 29px;
+  ${Wrapper}:hover & {
+    filter: hue-rotate(10deg) brightness(140%) saturate(80%);
+  }
 `
 
 const Ring = styled.img`
   position: absolute;
   top: -15px;
   left: -16px;
+
+  ${Wrapper}:hover & {
+    // in order to select an element in this way it must have been already declared
+    transform: rotate(30deg) scale(1.2) translate(1px, 1px);
+  }
+`
+
+const Icon = styled.img`
+  width: 29px;
+  height: 29px;
 `
